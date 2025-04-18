e0339494b6651ab63e198f3671aee4cc833cf1ee
XLEB-YSHEK <andreikorbanl@gmail.com>
2025-04-18 12:44:10 +0700

    Почти готовая версия

--- app/src/main/java/com/example/mynote/ui/SearchAdapter.kt
+++ app/src/main/java/com/example/mynote/ui/SearchAdapter.kt
@@ -1,8 +1,10 @@
 import android.content.Context
+import android.graphics.Paint
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.ImageView
 import android.widget.TextView
 import com.example.mynote.R
 import database.NoteEntity
@@ -17,9 +19,30 @@ class NoteAdapter(
             .inflate(R.layout.note_item, parent, false)
 
         val note = getItem(position) ?: return view
+        val markImageView = view.findViewById<ImageView>(R.id.note_item_mark)
+        val title = view.findViewById<TextView>(R.id.note_item_title)
 
-        view.findViewById<TextView>(R.id.note_item_title).text = note.title
-        //view.findViewById<TextView>(R.id.etview_description).text = note.description
+        title.text = note.title
+
+        when (note.state) {
+            0 -> {
+                markImageView.setImageResource(R.drawable.check_mark_button)
+                title.paintFlags = title.paintFlags or Paint.STRIKE_THRU_TEXT_FLAG
+                note.state = 1
+            }
+
+            1 -> {
+                markImageView.setImageResource(R.drawable.cross_mark_button)
+                title.paintFlags =
+                    title.paintFlags and Paint.STRIKE_THRU_TEXT_FLAG.inv()
+                note.state = 2
+            }
+
+            2 -> {
+                markImageView.setImageResource(R.drawable.white_medium_square)
+                note.state = 0
+            }
+        }
 
         return view
     }
